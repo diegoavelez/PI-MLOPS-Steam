0e6b5936426ebd347a050ce617620fb3ecfd1a23
diegoavelez <d.alejandrovelez@gmail.com>
2023-12-02 18:15:26 -0500

    Primer commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Modelo de ML para crear un modelo de recomendación de videojuegos para usuarios',
+    author='diegoavelez',
+    license='',
+)
